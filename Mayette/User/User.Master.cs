d62340ebe495f30ba738d10977d2c94b2a86ff4e
David Polo Abrugena <dpbabrugena@gmail.com>
2024-03-06 21:25:45 +0800

    Add project files.

---
+++ Mayette/User/User.Master.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mayette.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Request.Url.AbsoluteUri.ToString().Contains("Default.aspx")) 
+            {
+                form1.Attributes.Add("class", "sub_page");
+            }
+            else 
+            {
+                // Load the Control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+
+                // Add the Control to the Panel
+                pnlSliderUC.Controls.Add(sliderUserControl);
+            }
+        }
+    }
+}
